9551308ab451e3a915e14e8713c8f8f646d6399d
rileychapman <rlychpmn@gmail.com>
2013-12-18 00:10:14 -0500

    xaxis

--- driver.m
+++ driver.m
@@ -1,6 +1,7 @@
 resolution = 1000;
+runTime = 40;
 Dist = zeros([1, resolution]);
-time = linspace(.5,30,resolution);
+time = linspace(.5,runTime,resolution);
 for i = 1:length(time)
 	currentTime = time(i);
   	Dist(i) = doublePen(currentTime);
@@ -9,3 +10,4 @@ plot(time, Dist)
 title('Distanced Launched', 'fontsize', 28)
 xlabel('Time of Release (seconds)', 'fontsize', 15)
 ylabel('Distance Traveled', 'fontsize', 15)
+plot([0 runTime], [0 0], 'k-');
