47509e7e6e06c82f03e111103ba0368cb0412abe
rileychapman <rlychpmn@gmail.com>
2013-12-18 18:53:01 -0500

    separated animate

--- doublePen.m
+++ doublePen.m
@@ -96,33 +96,7 @@ function res = doublePen(time)
 %             legend('Theta1','Theta1Dot','Theta2','Theta2Dot')
 %         end
 
-        function animate(t,W)
-
-            %animate the pendulum
-
-
-
-            for i = 1:30:length(t)-2
-
-            clf;
-            hold on;
-            plot(x1(i),y1(i),'ro', 'Markersize', 12, 'MarkerFaceColor', 'r')
-            plot(x2(i), y2(i), 'bo', 'Markersize', 12, 'MarkerFaceColor', 'b')
-            line([0 x1(i)] ,[0 y1(i)]);
-            line([x1(i) x2(i)], [y1(i) y2(i)]);
-            xarrow = [x2(i), x2(i) + (x2(i+1)-x2(i))*150];
-            yarrow = [y2(i), y2(i) + (y2(i+1)-y2(i))*150];
-            plot(xarrow, yarrow)
-            axis([-L1-L2-10 L1+L2+10 -L1-L2-10 L1+L2+10]);
-
-
-
-            drawnow;
-            end
-
-
-
-        end
+       
 
 
 end   
