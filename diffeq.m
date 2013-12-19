43897a671f11756633081cd2fd487a9fa6331aaa
gubbatuba <subhash_gubba@yahoo.com>
2013-12-18 23:20:02 -0500

    okie dokie let's do this dammit

--- diffeq.m
+++ diffeq.m
@@ -1,10 +1,10 @@
 function res = diffeq(t,x)
             
-            g = x(:,5);
-            m1 = x(:,6);
-            m2 = x(:,7);
-            L1 = x(:,8);
-            L2 = x(:,9);
+            g = x(5);
+            m1 = x(6);
+            m2 = x(7);
+            L1 = x(8);
+            L2 = x(9);
 
             %the equations
             thetaDot1 = x(2);
@@ -17,7 +17,7 @@ function res = diffeq(t,x)
 
             thetaDDot2 = (((m1+m2)*(L1*x(2)^2+g*cos(x(1)))+L2*m2*x(4)^2*cos(x(1)-x(3)))*...
                         sin(x(1)-x(3)))/(L2*(m1+m2-m2*cos(x(1)-x(3))^2));
-
-            res = [thetaDot1;thetaDDot1;thetaDot2;thetaDDot2,0,0,0,0,0];        
+            
+            res = [thetaDot1;thetaDDot1;thetaDot2;thetaDDot2;0;0;0;0;0];        
 
         end
--- doublePenAnimatePrep.m
+++ doublePenAnimatePrep.m
@@ -1,4 +1,4 @@
-function res = doublePenAnimatePrep(launchTime)
+function res = doublePenAnimatePrep(runTime, launchTime)
  %initial conditions
     theda1Init = -pi/3; %radians
     thedaDot1Init = 0; %radians/sec
@@ -12,4 +12,33 @@ function res = doublePenAnimatePrep(launchTime)
     
     Minit = [theda1Init,thedaDot1Init,theda2Init,thedaDot2Init, g, m1, m2, L1, L2];
 
-    [t,M] = ode45(@diffeq, [0:.001:40] , Minit);
+    [t,M] = ode45(@diffeq, [0:.001:runTime] , Minit);
+    
+    x1 = L1 * sin(M(:,1));
+    y1 = L1 * cos(M(:,1));
+    x2 = x1 + L2* sin(M(:,3));
+    y2 = y1 + L2* cos(M(:,3));
+
+    launchTimeIndex = launchTime * 1000;
+    
+    startPosX = x2(launchTimeIndex);
+    startPosY = y2(launchTimeIndex);
+    startVelX = L1 * M(launchTimeIndex,2)*cos(M(launchTimeIndex,1)) + L2 * M(launchTimeIndex,4) * cos(M(launchTimeIndex,3));
+    startVelY = -L1 * M(launchTimeIndex,2)*sin(M(launchTimeIndex,1)) - L2 * M(launchTimeIndex,4) * sin(M(launchTimeIndex,3));
+
+    Derp = [startPosX; startPosY; startVelX; startVelY];
+
+
+    options = odeset('Events', @events);
+
+    function [value,isterminal,direction] = events(t,X)
+        value = X(2) + 20; %extract the current distance
+        isterminal = 1; %stop if the distance crossesn something
+        direction = -1; %but only if the distance is increasing
+    end
+
+    [t2, M2] = ode45(@trajectory, [0 50], Derp, options);
+    
+    
+end
+    
